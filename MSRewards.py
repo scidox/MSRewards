d41b1edf775eac990d3375b50b1de007a61c4ae3
Giorgio Attorresi <attorresi.giorgio@gmail.com>
2021-06-16 11:46:54 +0200

    Update MSRewards.py
    add comment

--- MSRewards.py
+++ MSRewards.py
@@ -8,12 +8,15 @@ import selenium.webdriver as webdriver
 
 #login in your bing account manually on edge and chrome
 
+#insert address edge file location
 edge="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 webbrowser.register('edge', None,webbrowser.BackgroundBrowser(edge))
 
 user_agent='Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1'
+#if you want to change user agent open firefox> Ctrl+Alt+m and choose the new user_agent
 profile= webdriver.FirefoxProfile()
 profile.set_preference('general.useragent.override',user_agent)
+#insert in the folder geckodriver.exe, download from https://github.com/mozilla/geckodriver/releases
 FireFoxDriverPath= os.path.join(os.getcwd(), 'Drivers',r"C:\Users\Giorgio\Desktop\py\geckodriver.exe")
 url='https://www.bing.com'
 browser=webdriver.Firefox(executable_path=FireFoxDriverPath,firefox_profile=profile)
